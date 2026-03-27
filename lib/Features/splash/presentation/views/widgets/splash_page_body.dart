436a548b2030f7fedeb716a3be4e82ae164c9b2c
mohamedtaha910 <mt164371@gmail.com>
2026-03-27 19:25:01 +0200

    add animation to spalsh_view

--- lib/Features/splash/presentation/views/widgets/splash_page_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_page_body.dart
@@ -1,31 +1,171 @@
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+// // import 'package:get/get_connect/http/src/utils/utils.dart';
+// // import 'package:get/get_connect/http/src/utils/utils.dart';
 
-class SplashPageBody extends StatelessWidget {
+class SplashPageBody extends StatefulWidget {
   const SplashPageBody({super.key});
+
+  @override
+  State<SplashPageBody> createState() => _SplashPageBodyState();
+}
+
+// class _SplashPageBodyState extends State<SplashPageBody>
+//     with SingleTickerProviderStateMixin {
+//   late AnimationController animationController;
+//   late Animation<Offset> slidingAnimation;
+
+//   @override
+//   void initState() {
+    
+
+//     animationController = AnimationController(
+//       vsync: this,
+//       duration: const Duration(seconds: 2),
+//     );
+
+//     slidingAnimation = Tween<Offset>(
+//       begin: const Offset(0, 4),
+//       end: Offset.zero,
+//     ).animate(animationController);
+
+//     slidingAnimation.addListener(() {
+//       setState(() {});
+//     });
+//     super.initState();
+//   }
+
+//   @override
+//   void dispose() {
+//     animationController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       crossAxisAlignment: CrossAxisAlignment.center,
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       children: [
+//         // Spacer(),
+//         Row(
+//           children: [
+//             Spacer(),
+//             SvgPicture.asset(
+//               AssetsPath.books,
+//               height: 70,
+
+//               // color: const Color.fromARGB(255, 57, 4, 99),
+//               // color: Colors.deepPurple.shade300,
+//             ),
+//             SizedBox(width: 16),
+//             Image.asset(AssetsPath.logo, width: 210),
+//             Spacer(),
+//           ],
+//         ),
+//         SizedBox(height: 8),
+//         AnimatedBuilder(
+//           animation: slidingAnimation,
+//           builder:(context, _) =>
+//            SlideTransition(
+//             position: slidingAnimation,
+            
+//             child: Text(
+//               '    Read Free Books',
+//               style: TextStyle(
+//                 fontWeight: FontWeight.w500,
+//                 fontStyle: FontStyle.italic,
+//                 color: const Color.fromARGB(255, 228, 200, 251),
+//               ),
+//             ),
+//           ),
+//         ),
+//         // Spacer(),
+//       ],
+//     );
+//   }
+// }
+
+// ========================================
+class _SplashPageBodyState extends State<SplashPageBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  late Animation<double> fadeAnimation;
+
+  @override
+  void initState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 4),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: animationController,
+        curve: Curves.easeOutBack, // 
+      ),
+    );
+
+    fadeAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(animationController);
+
+    animationController.forward();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        // Spacer(),
         Row(
           children: [
             Spacer(),
             SvgPicture.asset(
               AssetsPath.books,
-              height: 50,
-              color: const Color.fromARGB(255, 71, 6, 122),
+              height: 70,
             ),
             SizedBox(width: 16),
-            Image.asset(AssetsPath.logo, height: 200, width: 200),
+            Image.asset(AssetsPath.logo, width: 210),
             Spacer(),
           ],
         ),
-        // Spacer(),
+        SizedBox(height: 12),
+
+        AnimatedBuilder(
+          animation: animationController,
+          builder: (context, _) => FadeTransition(
+            opacity: fadeAnimation,
+            child: SlideTransition(
+              position: slidingAnimation,
+              child: Text(
+                'Read Free Books',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  fontStyle: FontStyle.italic,
+                  color: Color(0xffE4C8FB)
+                  ,
+                ),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,6 +1,7 @@
 class AssetsPath {
   static const String logo = 'assets/photo/Logo.png';
-  static const String books = 'assets/icons/boooks.svg';
+  static const String books = 'assets/icons/bookss.svg';
+  static const String books2 = 'assets/icons/boooks.svg';
   static const String book = 'assets/icons/book.svg';
 
 }
