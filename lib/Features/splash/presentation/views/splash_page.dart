28902871c48c9525af4e75809c98b6ead58c87f1
mohamedtaha910 <mt164371@gmail.com>
2026-03-27 14:59:30 +0200

    first commit

---
+++ lib/Features/splash/presentation/views/splash_page.dart
@@ -0,0 +1,14 @@
+import 'package:bookly_app/Features/splash/presentation/views/widgets/splash_page_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplashPageBody()
+    );
+     
+  }
+}
---
+++ lib/Features/splash/presentation/views/widgets/splash_page_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SplashPageBody extends StatelessWidget {
+  const SplashPageBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      
+    );
+  }
+}
